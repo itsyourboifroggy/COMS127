610530b81bf9fc8a49c42e81748d657de2fcf8ae
itsyourboifroggy <jackson.byboth@gmail.com>
2024-03-06 00:03:41 -0600

    added part 2
    finish in morning? or dont im so burnt out :3

---
+++ labWeek7/palindromeList.py
@@ -0,0 +1,24 @@
+# Jack Byboth Section 2 3/5/24
+
+
+
+
+
+
+
+
+
+
+def main():
+    while pal != "*":     # checks if s != *
+        pal = input("Make a list of palindromes, end with *:\n")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
