417bc0f23efc5ad2ce075d05fd2aca3840640da4
itsyourboifroggy <jackson.byboth@gmail.com>
2024-04-02 13:57:53 -0500

    started on lab

---
+++ labWeek11/wordSwap.py
@@ -0,0 +1,26 @@
+# Jack Byboth section 2 4/2/24
+# swaps words in a sentence using a key corresponding to the word
+
+def uInput():
+    words = input("Enter a list of words (more than one): ")
+    listWords = []
+    
+    listWords.append(words)
+    
+    print(listWords)
+
+
+def main():
+    uInput()
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
