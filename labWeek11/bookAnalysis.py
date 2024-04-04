68bf268502d0a9d0a77cef7efc8b6f737880d673
itsyourboifroggy <157855940+itsyourboifroggy@users.noreply.github.com>
2024-04-04 14:13:43 -0500

    worked on book thing

---
+++ labWeek11/bookAnalysis.py
@@ -0,0 +1,51 @@
+# Jack byboth 4/2/24
+# analyzes a book 
+
+
+with("36-0.txt", "r") as file:
+    file.readlines()
+    
+
+
+
+
+def removeP(a):
+
+
+
+    for word in a.split():
+
+        word = word.replace('_', '').replace('"', '').replace(',', '').replace('.', '')
+        word = word.replace('-', '').replace('?', '').replace('!', '').replace("'", "")
+        word = word.replace('(', '').replace(')', '').replace(':', '').replace('[', '')
+        word = word.replace(']', '').replace(';', '')
+
+    word = word.lower()
+
+def outputAnalysis():
+
+    count = {}
+    if word.isalpha():
+        if word in count:
+            count[word] = count[word] + 1
+        else:
+            count[word] = 1
+    keys = list(count.keys())
+    keys.sort()
+
+    out = open('allWords.txt', 'w')
+    for word in keys:
+        out.write(word + " " + str(count[word]))
+        out.write('\n')
+def getInput():
+    var = input("input name of txt file: ")
+    return var
+
+
+def main():
+    removeP(getInput)
+
+
+
+if __name__ == "__main__":
+    main()
