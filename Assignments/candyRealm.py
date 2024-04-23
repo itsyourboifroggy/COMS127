e8f2f9ff8d34a5cc2cb0145fbe46d3a77a05bf60
itsyourboifroggy <157855940+itsyourboifroggy@users.noreply.github.com>
2024-04-23 15:41:11 -0500

    worked on game and cards functions

--- Assignments/candyRealm.py
+++ Assignments/candyRealm.py
@@ -22,37 +22,89 @@ def printTitleMaterial():
     print("Section 2")
     print()
 
-def board():
-    from colorama import init, Fore, Back
+
 
 def board():
     init()
     boardList = ['M R B M C G B Y C B G Y G C Y R M R']
-    for char in boardList[0]:
-        if char == 'R':
-            print(Back.RED + char, end='')
-        elif char == 'G':
-            print(Back.GREEN + char, end='')
-        elif char == 'B':
-            print(Back.BLUE + char, end='')
-        elif char == 'Y':
-            print(Back.YELLOW + char, end='')
-        elif char == 'C':
-            print(Back.CYAN + char, end='')
-        elif char == 'M':
-            print(Back.MAGENTA + char, end='')
+    for i in boardList[0]:
+        if i == 'R':
+            print(Back.RED + i, end='')
+        elif i == 'G':
+            print(Back.GREEN + i, end='')
+        elif i == 'B':
+            print(Back.BLUE + i, end='')
+        elif i == 'Y':
+            print(Back.YELLOW + i, end='')
+        elif i == 'C':
+            print(Back.CYAN + i, end='')
+        elif i == 'M':
+            print(Back.MAGENTA + i, end='')
         else:
-            print(Fore.WHITE + char, end='')
+            print(Fore.WHITE + i, end='')
     print(Back.RESET + Fore.RESET)  
+
+
+def cards(rand, numCards):
+    cardList = ['R B M Y G C'] 
+    
+    for i in cardList[0]:
+        if i == 'R':
+            print(Back.RED + i, end='')
+        elif i == 'G':
+            print(Back.GREEN + i, end='')
+        elif i == 'B':
+            print(Back.BLUE + i, end='')
+        elif i == 'Y':
+            print(Back.YELLOW + i, end='')
+        elif i == 'C':
+            print(Back.CYAN + i, end='')
+        elif i == 'M':
+            print(Back.MAGENTA + i, end='')
+        elif rand == True:
+            random.shuffle(cardList)
+        else:
+            print(Fore.WHITE + i, end='')
+    print(Back.RESET + Fore.RESET)
+def players(playerCount):
+    playerCount = input('How many players 1-4?: ')
+    gameYN = True
+    players = ''
+    while gameYN:
+        if playerCount == '1':
+            players = '1'
+        elif playerCount == '2':
+            players = '2'    
+        elif playerCount == '3':
+           players = '3'
+        elif playerCount == '4':
+            players = '4'
+        else:
+            print('Enter a valid player number!')
+            gameYN = False
+    return players
+def theGame(players):
+
+    board()
+    # cards(rand)
 def mainMenu():
     
     thing = True
     while thing:    
-        menuChoice = str(input("Main Menu!: [p]lay game, [i]nstructions, or [q]uit "))
+        menuChoice = str(input("Main Menu!: [p]lay game, [i]nstructions, or [q]uit: "))
        
         if menuChoice == 'p':
-            board()
-                
+            
+            playerCount = int(input('How many players 1-4?: '))
+            numCards = int(input('How Many Copies Of Each Card?[1]-[5]: '))
+            if not 1 <= numCards <= 5:
+                print('Error enter a number 1-5')    
+            else:
+                cards(False, numCards)
+            if not 1 <= numCards <= 5:
+                print('Error enter a player count 1-4')    
+            else:
+                players(playerCount)
         elif menuChoice == 'i':
             print('This is a game called candy realm! play with 1-4 humans otherwise it will be bots. Draw a card and move to that color. Pretty Simple!')
                 
@@ -70,6 +122,7 @@ def mainMenu():
 
 
 def main():
+
     printTitleMaterial()
 
     mainMenu()
